bf73717e0054f47720246bc0500af765034749dd
Deborah Seow <deborahseow9@gmail.com>
2018-12-16 16:04:22 -0600

    Commented out liking from flatiron test

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import Button from './Button'
 
 const Message = ({id, content, author}) => {
   return(
     <div>
         <i className="message-author">{author.username}:</i> <span className="message-content">{content}</span>
-        <Button />
     </div>
   )
 }
 
+/*
+  <Button />
+*/
 export default Message
