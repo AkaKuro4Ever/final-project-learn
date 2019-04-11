54e5be48ad72507ddada6cfea2bd94d61842522a
Deborah Seow <deborahseow9@gmail.com>
2019-04-10 20:35:47 -0500

    Added test

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -3,7 +3,8 @@ import '../Styling.css'
 
 const Welcome = () => {
   return (
-    <div className="Welcome">Welcome to the Not-Instant Message Chatroom!</div>
+    <div className="Welcome">Welcome to the Not-Instant Message Chatroom!
+    Test 1, 2, 3</div>
   )
 }
 
